bb6e8ea6c3f8fafcd25e54b20479fee9042965a8
Patrick Frenett <patrick@frenett.net>
2025-03-22 12:44:28 +0000

    feat: actions v1

--- src/env/server.ts
+++ src/env/server.ts
@@ -7,10 +7,6 @@ export const serverEnv = createEnv({
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
     HOST: z.string(),
     // postgres
-    DATABASE_NAME: z.string().min(1),
-    DATABASE_USERNAME: z.string().min(1),
-    DATABASE_PASSWORD: z.string().min(1),
-    DATABASE_PORT: z.string().min(1),
     DATABASE_URL: z.string().url(),
   },
   experimental__runtimeEnv: process.env,
